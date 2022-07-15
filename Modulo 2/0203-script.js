0b6f71ba8398104e73c06154b051a1a8acbb20d1
Xavier <brunovx6@gmail.com>
2022-07-15 12:59:56 -0300

    MODULO2-feat: tarefa de operadores + tipos de dados

---
+++ Modulo 2/0203-script.js
@@ -0,0 +1,30 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+var expressao1 = 'x' / 2
+var expressao2 = 'x' - 2
+
+//Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50
+console.log(soma)
+
+//Incremente o número 5 e retorne o seu valor incrementado
+var x = 5
+console.log(++x)
+
+// Como dividir o peso por 2?
+var numero = '80'
+var unidade = 'kg'
+var numero = +'80' / 2
+var peso = numero + unidade
+var pesoPorDois = peso / 2
+
+var unidade = 'kg'
+var peso = numero + unidade
+
+console.log(peso)
+if(asA){
+var Faddasd
+}
