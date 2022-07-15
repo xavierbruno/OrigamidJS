f2a9a0b033a26f6a57188f1c789c5eaae5387138
Xavier <brunovx6@gmail.com>
2022-07-15 12:58:25 -0300

    MODULO2-feat: tarefa de variaveis

---
+++ Modulo 2/0201-script.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+
+var nome = "Bruno"
+
+// DEclarar uma variável com sua idade
+
+var idade = 27
+
+// Declarar uma variável com sua comida
+// favorita e não atribuir valor
+
+var comida 
+
+// Atibuir valor a sua comida favorita
+
+comida = "pizza"
+
+// Declarar 5 varáveis diferente sem valores
+
+var a, b, c, d, e
